420eb990e9545e0e0aacd5d35c8643970c6fdb6a
Dylan Reynolds <c2022508@hallam.shu.ac.uk>
2024-02-21 12:31:15 +0000

    error fix

--- occupational_health/lib/pages/home_page.dart
+++ occupational_health/lib/pages/home_page.dart
@@ -12,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   void signOut() async {
     final AuthService authService = AuthService();
     try {
@@ -162,14 +161,13 @@ class _HomePageState extends State<HomePage> {
                               color: Colors.orangeAccent,
                               borderRadius: BorderRadius.circular(10.0),
                             ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
                                     child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
+                                      mainAxisAlignment: MainAxisAlignment.center,
                                       crossAxisAlignment:
                                           CrossAxisAlignment.start,
                                       children: [
@@ -190,10 +188,7 @@ class _HomePageState extends State<HomePage> {
                                       ],
                                     ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Expanded(
+                                  Container(
                                     child: MySubmitButton(
                                       onPressed: () {},
                                       minWidth: 10,
@@ -202,8 +197,8 @@ class _HomePageState extends State<HomePage> {
                                       // need to style this text more
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         ),
