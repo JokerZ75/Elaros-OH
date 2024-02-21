02417c71b166d954e81b9e4d0dd3c81a1e5eb7f2
Dylan Reynolds <c2022508@hallam.shu.ac.uk>
2024-02-21 14:35:52 +0000

    main update

--- occupational_health/lib/main.dart
+++ occupational_health/lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       ),
       title: 'Occupational Health',
       debugShowCheckedModeBanner: false,
-      home: const SupportPage(),
+      home: const ListViewMain(),
     );
   }
 }
