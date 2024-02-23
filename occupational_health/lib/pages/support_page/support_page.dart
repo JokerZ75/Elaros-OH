79c8f23a624767f462db898f684a5c8b5809c572
Dylan Reynolds <c2022508@hallam.shu.ac.uk>
2024-02-23 11:19:30 +0000

    map progess

--- occupational_health/lib/pages/support_page/support_page.dart
+++ occupational_health/lib/pages/support_page/support_page.dart
@@ -2,6 +2,8 @@ import "package:flutter/material.dart";
 import "package:occupational_health/components/my_submit_button.dart";
 import "package:occupational_health/pages/support_page/components/my_rehab_content_card.dart";
 import "package:url_launcher/url_launcher_string.dart";
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class SupportPage extends StatefulWidget {
   const SupportPage({Key? key}) : super(key: key);
@@ -35,13 +37,15 @@ class _SupportPageState extends State<SupportPage> {
                   MyRehabCard(
                     title: "Breathlessness",
                     onPressed: () {
-                      launchUrlString('https://www.yourcovidrecovery.nhs.uk/i-think-i-have-long-covid/effects-on-your-body/breathlessness/');
+                      launchUrlString(
+                          'https://www.yourcovidrecovery.nhs.uk/i-think-i-have-long-covid/effects-on-your-body/breathlessness/');
                     },
                   ),
                   MyRehabCard(
                     title: "Fatigue",
                     onPressed: () {
-                      launchUrlString('https://www.yourcovidrecovery.nhs.uk/i-think-i-have-long-covid/effects-on-your-body/fatigue/');
+                      launchUrlString(
+                          'https://www.yourcovidrecovery.nhs.uk/i-think-i-have-long-covid/effects-on-your-body/fatigue/');
                     },
                   ),
 
@@ -51,23 +55,22 @@ class _SupportPageState extends State<SupportPage> {
                     color: const Color(0xFFEFB84C),
                     child: Padding(
                       padding: const EdgeInsets.all(5.0),
-                        child: Row(
-                          children: <Widget>[
-                            // Button linking for more
-                            Expanded(
-                                child: MySubmitButton(
-                              onPressed: () {},
-                              text: "Click Here For More",
-                              minWidth: 110,
-                              lineHeight: 20,
-                              textSize: 14,
-                              fontWeight: FontWeight.w600,
-                            )),
-                          ],
-                        ),
+                      child: Row(
+                        children: <Widget>[
+                          // Button linking for more
+                          Expanded(
+                              child: MySubmitButton(
+                            onPressed: () {},
+                            text: "Click Here For More",
+                            minWidth: 110,
+                            lineHeight: 20,
+                            textSize: 14,
+                            fontWeight: FontWeight.w600,
+                          )),
+                        ],
                       ),
                     ),
-                  
+                  ),
                 ],
               ),
             ),
@@ -75,6 +78,57 @@ class _SupportPageState extends State<SupportPage> {
             const SizedBox(height: 30),
             const Text("Locate your nearest clinic"),
             const SizedBox(height: 15),
+            Container(
+              width: 350,
+              height: 350,
+              child: 53.383331 != null && -1.466667 != null
+            ? Stack(
+                children: [
+                  FlutterMap(
+                    options: MapOptions(
+                      center: LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
+                      zoom: 14,
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                      CircleLayer(
+                        circles: [
+                          CircleMarker(
+                            point: LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
+                            radius: 350,
+                            useRadiusInMeter: true,
+                            color: Colors.blue.withOpacity(0.2),
+                            borderColor: Colors.red.withOpacity(0.7),
+                            borderStrokeWidth: 2,
+                          )
+                        ],
+                      ),
+                      MarkerLayer(
+                        markers: [
+                          // Marker(
+                          //   point: LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
+                          //   width: 80,
+                          //   height: 80,
+                          //   child: Icon(
+                          //     Icons.location_pin,
+                          //     color: Colors.red.withOpacity(0.9),
+                          //     size: 20,
+                          //   ),
+                          // ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
           ],
         ),
       ),
