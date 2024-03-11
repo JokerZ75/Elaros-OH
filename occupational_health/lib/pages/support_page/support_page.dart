1508140240a48e1a7d891fe123dd3bce54671d49
2022508 <c2022508@hallam.shu.ac.uk>
2024-03-11 09:38:01 +0000

    progress

--- occupational_health/lib/pages/support_page/support_page.dart
+++ occupational_health/lib/pages/support_page/support_page.dart
@@ -4,6 +4,7 @@ import "package:occupational_health/pages/support_page/components/my_rehab_conte
 import "package:url_launcher/url_launcher_string.dart";
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class SupportPage extends StatefulWidget {
   const SupportPage({Key? key}) : super(key: key);
@@ -13,8 +14,55 @@ class SupportPage extends StatefulWidget {
 }
 
 class _SupportPageState extends State<SupportPage> {
+  double? longitude;
+  double? latitude;
+  double? initialZoom = 14;
+
+  void getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        forceAndroidLocationManager: true,
+        desiredAccuracy: LocationAccuracy.best);
+    setState(() {
+      longitude = position.longitude;
+      latitude = position.latitude;
+    });
+    Text("Longitude: $longitude, Latitude: $latitude");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (longitude != null && latitude != null) {
+      longitude = 0;
+      latitude = 0;
+      initialZoom = 0;
+    }
     return Scaffold(
         body: SingleChildScrollView(
       child: Padding(
@@ -81,53 +129,60 @@ class _SupportPageState extends State<SupportPage> {
             Container(
               width: 350,
               height: 350,
-              child: 53.383331 != null && -1.466667 != null
-            ? Stack(
-                children: [
-                  FlutterMap(
-                    options: MapOptions(
-                      center: LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
-                      zoom: 14,
+
+              // child: 53.383331 != null && -1.466667 != null
+              child: longitude != null && latitude != null
+                  ? Stack(
+                      children: [
+                        FlutterMap(
+                          options: MapOptions(
+                            initialCenter:
+                                // LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
+                                LatLng(longitude ?? 0.0, latitude ?? 0.0),
+                            initialZoom: initialZoom ?? 0.0,
+                          ),
+                          children: [
+                            TileLayer(
+                              urlTemplate:
+                                  'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                              userAgentPackageName: 'com.example.app',
+                            ),
+                            CircleLayer(
+                              circles: [
+                                CircleMarker(
+                                  point: LatLng(
+                                      // 53.383331 ?? 0.0, -1.466667 ?? 0.0),
+                                      longitude ?? 0.0,
+                                      latitude ?? 0.0),
+                                  radius: 350,
+                                  useRadiusInMeter: true,
+                                  color: Colors.blue.withOpacity(0.2),
+                                  borderColor: Colors.red.withOpacity(0.7),
+                                  borderStrokeWidth: 2,
+                                )
+                              ],
+                            ),
+                            MarkerLayer(
+                              markers: [
+                                // Marker(
+                                //   point: LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
+                                //   width: 80,
+                                //   height: 80,
+                                //   child: Icon(
+                                //     Icons.location_pin,
+                                //     color: Colors.red.withOpacity(0.9),
+                                //     size: 20,
+                                //   ),
+                                // ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  : Center(
+                      child: CircularProgressIndicator(),
                     ),
-                    children: [
-                      TileLayer(
-                        urlTemplate:
-                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                        userAgentPackageName: 'com.example.app',
-                      ),
-                      CircleLayer(
-                        circles: [
-                          CircleMarker(
-                            point: LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
-                            radius: 350,
-                            useRadiusInMeter: true,
-                            color: Colors.blue.withOpacity(0.2),
-                            borderColor: Colors.red.withOpacity(0.7),
-                            borderStrokeWidth: 2,
-                          )
-                        ],
-                      ),
-                      MarkerLayer(
-                        markers: [
-                          // Marker(
-                          //   point: LatLng(53.383331 ?? 0.0, -1.466667 ?? 0.0),
-                          //   width: 80,
-                          //   height: 80,
-                          //   child: Icon(
-                          //     Icons.location_pin,
-                          //     color: Colors.red.withOpacity(0.9),
-                          //     size: 20,
-                          //   ),
-                          // ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
             ),
           ],
         ),
