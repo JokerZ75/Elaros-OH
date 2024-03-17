2900b5b2d6b370f666df41d41c8ea983408e777c
2022508 <c2022508@hallam.shu.ac.uk>
2024-03-17 12:03:09 +0000

    support page maps done

--- occupational_health/lib/pages/support_page/support_page.dart
+++ occupational_health/lib/pages/support_page/support_page.dart
@@ -2,9 +2,9 @@ import "package:flutter/material.dart";
 import "package:occupational_health/components/my_submit_button.dart";
 import "package:occupational_health/pages/support_page/components/my_rehab_content_card.dart";
 import "package:url_launcher/url_launcher_string.dart";
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import "package:flutter_google_maps_webservices/places.dart";
 
 class SupportPage extends StatefulWidget {
   const SupportPage({Key? key}) : super(key: key);
@@ -14,6 +14,8 @@ class SupportPage extends StatefulWidget {
 }
 
 class _SupportPageState extends State<SupportPage> {
+  final GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: "Enter Key");
+
   // https://www.dhiwise.com/post/maximizing-user-experience-integrating-flutter-geolocator
   // this website was used to help with the location services, both getting the location and displaying it on the map
   Future<Position> getLocation() async {
@@ -37,9 +39,24 @@ class _SupportPageState extends State<SupportPage> {
       return Future.error('Location permissions are permanently denied.');
     }
 
-    return await Geolocator.getCurrentPosition();
-    // forceAndroidLocationManager: true,
-    // desiredAccuracy: LocationAccuracy.best);
+    return await Geolocator.getCurrentPosition(
+        forceAndroidLocationManager: true,
+        desiredAccuracy: LocationAccuracy.best);
+  }
+
+  Future<List<PlacesSearchResult>> getNearbyHospitals(Position position) async {
+    PlacesSearchResponse response = await _places.searchNearbyWithRadius(
+      Location(lat: position.latitude, lng: position.longitude),
+      8047, // 5 miles in meters
+      type: "hospital",
+    );
+
+    if (response.status == "OK") {
+      return response.results;
+    } else {
+      throw Exception(
+          'Failed to fetch nearby restaurants.\nError: ${response.status}');
+    }
   }
 
   @override
@@ -105,76 +122,77 @@ class _SupportPageState extends State<SupportPage> {
             ),
 
             const SizedBox(height: 30),
-            const Text("Locate your nearest clinic"),
+            const Text("Locate your nearest hospital"),
             const SizedBox(height: 15),
 
             FutureBuilder(
               future: getLocation(),
               builder:
-                  (BuildContext context, AsyncSnapshot<Position> snapshot) {
-                if (snapshot.hasData) {
-                  return Container(
+                  (BuildContext context, AsyncSnapshot<Position> snapshot1) {
+                if (snapshot1.hasData) {
+                  return SizedBox(
                     width: 350,
                     height: 350,
                     child: Stack(
                       children: [
-                        FlutterMap(
-                          options: MapOptions(
-                            initialCenter: LatLng(
-                              snapshot.data!.latitude,
-                              snapshot.data!.longitude,
-                            ),
-                            initialZoom: 14,
-                          ),
-                          children: [
-                            TileLayer(
-                              urlTemplate:
-                                  'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                              userAgentPackageName: 'com.example.app',
-                            ),
-                            CircleLayer(
-                              circles: [
-                                CircleMarker(
-                                  point: LatLng(
-                                    snapshot.data!.latitude,
-                                    snapshot.data!.longitude,
+                        FutureBuilder(
+                          future: getNearbyHospitals(snapshot1.data!),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<List<dynamic>> snapshot2) {
+                            if (snapshot2.hasData) {
+                              Set<Marker> markers = {};
+                              for (var restaurant in snapshot2.data!) {
+                                markers.add(
+                                  Marker(
+                                    markerId: MarkerId(restaurant.placeId),
+                                    // markerId: MarkerId(restaurant.id),
+                                    position: LatLng(
+                                        restaurant.geometry.location.lat,
+                                        restaurant.geometry.location.lng),
+                                    infoWindow: InfoWindow(
+                                      title: restaurant.name,
+                                      snippet: restaurant.vicinity,
+                                    ),
+                                    icon: BitmapDescriptor.defaultMarkerWithHue(
+                                        BitmapDescriptor.hueBlue),
                                   ),
-                                  radius: 350,
-                                  useRadiusInMeter: true,
-                                  color: Colors.blue.withOpacity(0.2),
-                                  borderColor: Colors.red.withOpacity(0.7),
-                                  borderStrokeWidth: 2,
-                                )
-                              ],
-                            ),
-                            MarkerLayer(
-                              markers: [
+                                );
+                              }
+                              markers.add(
                                 Marker(
-                                  point: LatLng(
-                                    snapshot.data!.latitude,
-                                    snapshot.data!.longitude,
-                                  ),
-                                  width: 80,
-                                  height: 80,
-                                  child: Icon(
-                                    Icons.location_pin,
-                                    color: Colors.red.withOpacity(0.9),
-                                    size: 20,
-                                  ),
+                                    markerId: const MarkerId('currentLocation'),
+                                    position: LatLng(snapshot1.data!.latitude,
+                                        snapshot1.data!.longitude),
+                                    infoWindow: const InfoWindow(
+                                      title: 'Your Location',
+                                    ),
+                                    icon: BitmapDescriptor.defaultMarkerWithHue(
+                                        BitmapDescriptor.hueRed)),
+                              );
+                              return GoogleMap(
+                                initialCameraPosition: CameraPosition(
+                                  target: LatLng(snapshot1.data!.latitude,
+                                      snapshot1.data!.longitude),
+                                  zoom: 12,
                                 ),
-                              ],
-                            ),
-                          ],
+                                markers: markers,
+                              );
+                            } else if (snapshot2.hasError) {
+                              return Text('${snapshot2.error}');
+                            }
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          },
                         ),
+                        //
                       ],
                     ),
                   );
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot1.hasError) {
+                  return Text('Error: ${snapshot1.error}');
                 }
-
-                // CircularProgressIndicator while waiting for the location permission
-                return CircularProgressIndicator();
+                return const CircularProgressIndicator();
               },
             ),
           ],
